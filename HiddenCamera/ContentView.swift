392e85242c54f28d47e2f2212f05a43d30c272af
Linh Vu <linhvn2303@gmail.com>
2025-05-12 17:05:36 +0700

    Initial Commit

---
+++ HiddenCamera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HiddenCamera
+//
+//  Created by Linh Vu on 12/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HiddenCamera/HiddenCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiddenCameraApp.swift
+//  HiddenCamera
+//
+//  Created by Linh Vu on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct HiddenCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
